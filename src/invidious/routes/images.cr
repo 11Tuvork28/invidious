99c3b45018bb90af0a20b85769142fc526a67e8a
11tuvork28 <zofi@xamh.de>
2022-10-06 19:13:26 +0200

    fix vi images

--- src/invidious/routes/images.cr
+++ src/invidious/routes/images.cr
@@ -237,7 +237,7 @@ module Invidious::Routes::Images
 
     if name == "maxres.jpg"
       build_thumbnails(id).each do |thumb|
-        thumbnail_resource_path = "https://static.xamh.de/vi/#{id}/#{thumb[:url]}.jpg"
+        thumbnail_resource_path = "/vi/#{id}/#{thumb[:url]}.jpg"
         # Logic here is short enough that manually typing them out should be fine.
         {% unless flag?(:disable_quic) %}
           if CONFIG.use_quic
